f57792ecbc429d5c7729e8a4860e5a90562438ae
af0liveira <augustof.chem@gmail.com>
2022-09-13 14:06:51 +0200

    changed default variable values in execution scripts

--- bin/amsqtp.sh
+++ bin/amsqtp.sh
@@ -3,8 +3,8 @@
 # If AMS is correctly configured, this doesn't need to be changed
 py3="$AMSBIN/amspython"
 
-# Set up the path to the QTP packager directory
-srcpath="$HOME/00-PARA/02-Areas/Code/qtp/qtp"
+# Set up the path to the QTP package directory
+srcpath="$HOME/qtp/qtp"
 
 # Run the QTP program.
 # !!! You should not change it. !!!
--- bin/qtp.sh
+++ bin/qtp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Set up the correct Python interpreter.
-# py3="$HOME/opt/anaconda3/bin/python3"
+# Set up the correct Python interpreter, if necessary
 py3="python3"
 
-# Set up the path to the QTP packager directory
-srcpath="$HOME/00-PARA/02-Areas/Code/qtp/qtp"
+# Set up the path to the QTP package directory
+srcpath="$HOME/qtp/qtp"
 
 # Run the QTP program.
 # !!! You should not change it. !!!
